b2142b9ce18f1b81e3a56b4ae26e14a9721505ff
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-11 20:28:42 +0400

    update

--- HW_LinQ1/Program.cs
+++ HW_LinQ1/Program.cs
@@ -1,24 +1,22 @@
 ﻿var squad = new List<Soldier>
 {
-    new Soldier("John", "Desert Eagle", "Second lieutenant", 6),
-    new Soldier("Mike", "Desert Eagle", "Second lieutenant", 6),
-    new Soldier("Tobias", "M4-A1", "Captain", 12),
-    new Soldier("Mark", "M4-S", "Captain", 18),
-    new Soldier("Noah", "Sniper riffle", "Major", 24),
-    new Soldier("Ethan", "Sniper riffle", "Major", 30),
+    new("John", "Desert Eagle", "Second lieutenant", 6),
+    new("Mike", "Desert Eagle", "Second lieutenant", 6),
+    new("Tobias", "M4-A1", "Captain", 12),
+    new("Mark", "M4-S", "Captain", 18),
+    new("Noah", "Sniper riffle", "Major", 24),
+    new("Ethan", "Sniper riffle", "Major", 30),
 };
 
 var nameAndRank = from n in squad
-    select new { n.Name , n.Rank };
-
-Console.WriteLine();
+    select new { n.Name, n.Rank };
 
 Console.ForegroundColor = ConsoleColor.Green;
 foreach (var item in squad)
 {
     Console.WriteLine($"{item.Name} : {item.Rank}");
 }
+
 Console.ResetColor();
-Console.WriteLine();
 
 internal record class Soldier(string Name, string Weapon, string Rank, int LifeTime);
